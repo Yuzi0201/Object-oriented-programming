d971d698983bb329b1d362a049a1041d49bb84fd
Yuzi19 <2205816944@qq.com>
2021-03-29 19:47:14 +0800

    pay1.0

--- payment/payment.cpp
+++ payment/payment.cpp
@@ -6,9 +6,9 @@ class normal
 public:
 	normal(int x);
 	~normal();
-	int input() { cout << "Please input the basic salary: "; cin >> basic; }
-	int pay() { salary = basic; }
-	int print() { cout << "This worker's salary is:" << salary << endl; }
+	void input() { cout << "Please input the basic salary: "; cin >> basic; }
+	void pay() { salary = basic; }
+	void print() { cout << "This worker's salary is:" << salary << endl << endl << endl; }
 protected:
 	int basic;
 	int salary;
@@ -17,7 +17,6 @@ protected:
 normal::normal(int x=0)
 {
 	this->basic = x; this->salary = x;
-	cout << "This is normal worker A's salary" << endl;
 }
 
 normal::~normal()
@@ -29,9 +28,9 @@ class saler:public normal
 public:
 	saler(int x);
 	~saler();
-	static int Commission_rate; int input_rate() { cin >> Commission_rate; }
-	int input() { cout << "Please input the sales volume: "; cin >> sale; }
-	int pay() { salary = basic+sale*Commission_rate; }
+	static int Commission_rate; void input_rate() { cin >> Commission_rate; }
+	void salerinput() { input(); cout << "Please input the sales volume: "; cin >> sale; }
+	void pay() { salary = basic+sale*Commission_rate; }
 protected:
 	int sale;
 };
@@ -48,15 +47,17 @@ saler::~saler()
 class ceo:public saler
 {
 public:
-	ceo();
+	ceo( int x);
 	~ceo();
-
+	void ceoinput() { salerinput(); cout << "Please input the extra salary: "; cin >> extra; }
+	void pay() { salary = basic + sale * Commission_rate + extra; }
 private:
-
+	int extra;
 };
 
-ceo::ceo()
+ceo::ceo(int x=0)
 {
+	this->extra = x;
 }
 
 ceo::~ceo()
@@ -65,11 +66,29 @@ ceo::~ceo()
 
 int main()
 {
+	cout << "Welcome to the Wage calculation system!!" << endl;
 	cout << "Please enter the Commission rate:  ";
-	normal A; saler B;
-	B.input_rate;
-	A.input; A.pay; A.print;
-	B.input; B.pay; B.print;
-
+	saler test; test.input_rate();
+	normal A; saler B; ceo C;
+	while (true)
+	{
+		cout << "Please enter the number of employee type:" << endl;
+		cout << "1.Normal employeer 2.Saler 3.CEO" << endl;
+		int chooser; cin >> chooser;
+		switch (chooser)
+		{
+		case 1:A.input(); A.pay(); A.print(); break;
+		case 2:B.salerinput(); B.pay(); B.print(); break;
+		case 3:C.ceoinput(); C.pay(); C.print(); break;
+		default:cout << "Wrong input!Please enter 1,2 or 3!!" << endl;
+			break;
+		}
+		cout << "enter 1 to quit, 2 to continue:  "; int continue_chooser; cin >> continue_chooser;
+		cout << endl;
+		if (continue_chooser==1)
+		{
+			return 0;
+		}
+	}
 }
 
